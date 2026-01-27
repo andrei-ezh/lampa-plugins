c3f6c63c1258a0bfee6b5a7e0e32ac4c5e83939d
Andrei Ezhov <268ry7cgpg@privaterelay.appleid.com>
2026-01-27 17:39:04 +0300

    Add files via upload

---
+++ td/README.md
@@ -0,0 +1,2 @@
+# Lampa torrent downloader
+Small project help download torrent from Lampa to local client
---
+++ td/devtd.js
@@ -0,0 +1,6 @@
+(function () {
+	'use strict';
+
+	var hash = Lampa.Utils.hash(111);
+	Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/td/td.js'], function () { });
+})();
