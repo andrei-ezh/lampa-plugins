78fa925e7940ef99712b85af638f5d8b861b7a99
Andrei Ezhov <268ry7cgpg@privaterelay.appleid.com>
2026-01-27 20:33:45 +0300

    Delete td/devtd directory

--- td/devtd/devtd.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	'use strict';
-
-	//Commit for test workflow
-	var script = document.createElement('script');
-	script.src = 'https://lampame.github.io/main/td.js';
-	document.head.appendChild(script);
-
-})();
