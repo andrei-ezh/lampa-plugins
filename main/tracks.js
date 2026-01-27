ed69b12fd3026adbff333d9cc0f5e627778d27a4
Andrei Ezhov <268ry7cgpg@privaterelay.appleid.com>
2026-01-27 20:16:38 +0300

    Update script source URL in tracks.js

--- main/tracks.js
+++ main/tracks.js
@@ -1,4 +1,4 @@
 (function () {
     'use strict';
-    Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/tracks/tracks.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://andrei-ezh.github.io/lampa-plugins/main/tracks/tracks.js'], function () { });
 })();
